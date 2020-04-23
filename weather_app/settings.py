d91a44b5974be2ebfec99e269900c38b9623d414
Wojciech Popławski <wojciechpoplawski94@gmail.com>
2020-04-23 12:26:12 +0200

    Update settings.py

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=i6o(pplfb!e53gb6=pa)x5)t)6%*i2-za)1%$n@91-n63b598'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['theweatherchecker.herokuapp.com']
 
